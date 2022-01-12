735375824ed1f4a7b60da373bff1f9dc1ecb1beb
JuniorMira <j-unyor14@hotmail.com>
2022-01-12 12:40:44 -0300

    trybewarts

---
+++ Trybewarts/script.js
@@ -0,0 +1,27 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const buttonEntrar = document.getElementById("buttonEntrar");
+
+// função para checkar email e senha
+function checkUser() {
+  if (email.value == "tryber@teste.com" && password.value == "123456") {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+}
+
+buttonEntrar.addEventListener("click", checkUser);
+
+const agreement = document.getElementById("agreement");
+const submitButton = document.getElementById("submit-btn");
+
+submitButton.disabled = true;
+
+agreement.addEventListener("click", () => {
+  if (agreement.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
